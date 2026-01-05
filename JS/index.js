0a332546363cb32b8f082da5e2ab413af61b8b61
l-wanderer01 <jhjjang0423@naver.com>
2026-01-05 13:28:30 +0900

    js 기본 문법

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+console.log("Hello, Javascript!");
+document.write("<h1>Welcome to Javascript!</h1>");
+document.write("자바스크립트로 만든 문단");
+// window.alert("Hello, Javascript! alert");
+// 페이지 로딩될 때 경고창 표시
+// alert("Hello, Javascript! alert"); // window 생략 가능
+console.error("에러 메시지입니다."); // 콘솔에 에러 메시지 출력 (디버깅에서 사용)
+// location.href = "https://www.google.com"; // 앞의 JS 내용을 출력한 후 구글 홈페이지로 이동
+console.log(navigator.userAgent); // 현재 브라우저 정보 출력
+// "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36" 이와 같이 출력
